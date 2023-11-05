687032d283cf6dc7e6c40ba9dd91011905565857
Mr-Wang-Y-P <116936390+Mr-Wang-Y-P@users.noreply.github.com>
2023-11-05 22:41:48 +0800

    Update index.tsx

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import SliderLeft from "../SliderLeft";
 import { Outlet } from "react-router-dom";
 import styles from "./index.module.scss";
