02aa414a3cc852f05933c8e9488e67294e8949e1
Mr-Wang-Y-P <2647135548@qq.com>
2023-11-18 23:31:43 +0800

    feat-修改侧边栏的样式，outline等。

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import "./index.css";
+import "./index.scss";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router} />
 );
